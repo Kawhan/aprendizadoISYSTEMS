2dbe54017b8ca573388a155c24f9f69165621b91
Kawhan <kawhanlaurindo@outlook.com>
2022-02-22 07:38:26 -0300

    novas_alunas_js

---
+++ js-alura/fundamentos/tipagem.js
@@ -0,0 +1,7 @@
+// tipagem dinâmica
+// untyped
+// Multiparadigma --> resolver o mesmo problema de diversas formas diferentes
+// O que é o NodeJS --> O javascript como é uma linguagem interpretada ele precisa de um interpretador, podemos usar o browser ou o node como interepretador, quando utilizamos o nodejs casualmente usamos no lado do backend ou seja no lado do servidor
+
+let minhaVar = 567;
+minhaVar = "texto";
