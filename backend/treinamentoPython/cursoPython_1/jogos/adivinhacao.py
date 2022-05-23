df0f714b9fc9d398af8f7bb5d5d9359245740e90
Kawhan <kawhan@DESKTOP-1VQLM4V.localdomain>
2022-05-23 16:57:08 -0300

    commit_files_23/05/2022

---
+++ backend/treinamentoPython/cursoPython_1/jogos/adivinhacao.py
@@ -0,0 +1,58 @@
+import random
+
+print("################################")
+print("Bem vindo no jogo de adivinhacao")
+print("################################\n")
+
+numero_secreto = random.randrange(1, 101)
+numero_maior = 100
+numero_menor = 1
+total_tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícl")
+
+nivel = int(input("Digite o nível: "))
+
+while (nivel <= 0 or nivel >= 3):
+    print("\nNível invalido!")
+    print("(1) Fácil (2) Médio (3) Difícl")
+    nivel = int(input("\nDigite o nível: "))
+    
+if (nivel == 1):
+    total_tentativas = 20
+elif (nivel == 2):
+    total_tentativas = 10
+elif (nivel == 3):
+    total_tentativas = 5
+    
+
+for rodada in range(1,total_tentativas + 1):
+    print(f"\nTentativa: {rodada} de {total_tentativas}" )
+    chute_str  = input("Digite seu número entre 1 e 100: ")
+
+    chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+        
+    acertou  = chute ==  numero_secreto
+    maior = chute > numero_secreto
+    menor  = chute < numero_secreto
+    
+    if (acertou):
+        print("\nVocê acertou!")
+        break
+    else:
+        if (maior):
+            numero_maior = chute
+            print("\nVocê errou! Seu chute foi maior que o do número secreto!")
+            print(f"Seu número está entre {numero_menor} e {numero_maior}")
+        elif (menor):
+            numero_menor = chute
+            print("\nVocê errou! Seu chute foi menor que o do número secreto!")
+            print(f"Seu número está entre {numero_menor} e {numero_maior}")
+    
+
+print("\nFim do jogo!")
---
+++ backend/treinamentoPython/cursoPython_1/python_part1_introducao/saida.py
@@ -0,0 +1,20 @@
+print("Olá mundo")
+
+# Usado para separar
+print("Brasil", "ganhou", "5", "titulos", sep="-")
+
+#Usado no final do nosso print
+print("Teste para não pular linha", end="\n")
+
+# Variável e o tipo dela
+pais = "Brasil"
+quantidade = 5
+print(type(pais))
+print(type(quantidade))
+
+print(f"{pais} ganhou {quantidade} titulos mundiais")
+
+dia = 15
+mes = 10
+ano = 2015
+print(dia,mes,ano,sep="/")
