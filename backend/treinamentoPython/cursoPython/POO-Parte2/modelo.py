0422b75f1009672eaf5162fcbbf670a93fcaf00d
Kawhan <kawhan@DESKTOP-1VQLM4V.localdomain>
2022-06-01 06:57:11 -0300

    commit_files_01/06/2022

--- backend/treinamentoPython/cursoPython/POO-Parte2/modelo.py
+++ backend/treinamentoPython/cursoPython/POO-Parte2/modelo.py
@@ -1,55 +1,73 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
     
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
     
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter
     def nome(self, nome):
-        self.__nome = nome.title()
+        self._nome = nome.title()
     
     def dar_like(self):
-        self.__likes += 1
+        self._likes += 1
+    
+    def __str__(self):
+        return f'{self._nome} - {self.ano}: {self._likes} Likes' 
         
 
-class Serie:
+class Filme(Programa):
+    def __init__(self,nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+    
+    def __str__(self):
+        return f'{self._nome} - {self.ano}- {self.duracao} minutos -  {self._likes} Likes'
+
+
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
     
-    @property
-    def likes(self):
-        return self.__likes
-    
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome.title()
-    
-    def dar_like(self):
-        self.__likes += 1
+    def __str__(self):
+        return f'{self._nome} - {self.ano}- {self.temporadas} temporadas -  {self._likes} Likes'
 
-vingadores = Filme('vingadores = guerra infinita', 2018, 160) 
+# Posso fazer com que ela herde o comportamento de uma lista
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160) 
+atlanta = Serie("atlanta", 2018, 2)
+tmep = Filme('todo mundo em pânico', 1999, 100)
+demolidor  = Serie('Demolidor', 2016, 2)
+
+
 vingadores.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} minutos - Likes: {vingadores.likes}')    
-
-
-atlanta  = Serie("atlanta", 2018, 2)
 atlanta.dar_like()
-atlanta.nome = "the - boys"
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+atlanta.nome = "the boys"
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist("Fim de Semana", filmes_e_series)
+
+print(f'Tamanho da Playlist: {len(playlist_fim_de_semana)}')
+# Colocando o polimorfismo em pratica, como todos fazem parte de uma unica super classe não importa as subclasses que eles tem
+# Eles podem interar também de forma mais tranquila sem muitos ifs por causa da questão que cada um tem um método imprime
+for programa in playlist_fim_de_semana:
+    print(programa)
+
+
+print(demolidor in playlist_fim_de_semana)
