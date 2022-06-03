cf81825f14c2ae18a48459d1ef03c29ee154aefd
Kawhan <kawhan@DESKTOP-1VQLM4V.localdomain>
2022-06-03 13:44:44 -0300

    commit_files_03/06/2022

--- backend/treinamentoPython/cursoPython/projetoBiblioteca/livrosClass/modelo.py
+++ backend/treinamentoPython/cursoPython/projetoBiblioteca/livrosClass/modelo.py
@@ -7,7 +7,16 @@ class Livros:
     def __str__(self):
         return f'Nome do livro: {self._nome} - Autor: {self._autor} - Data publicação: {self._data_publicacao}'
     
-#
+    # 
+    def __eq__(self, outro_filme_nome):
+        return self._nome == outro_filme_nome
+    
+    def __ne__(self, outro_filme_nome):
+        return self._nome != outro_filme_nome
+    
+    @property
+    def nome(self):
+        return self._nome
     
     
 class Biblioteca():
@@ -23,7 +32,6 @@ class Biblioteca():
         
         return livros
         
-    
     def listarLivros(self):
         lista_livros = self.pegarLivros()
         print("\nListando os Livros\n")
@@ -39,16 +47,40 @@ class Biblioteca():
             nome = input("Digite o nome do seu livro: ")
             autor = input("Digite o autor do seu livro: ")
             data_de_publicacao = input("Digite a data de publicação do seu livro: ")
+            
+            nome = nome.title().strip()
+            autor = autor.title().strip()
+            data_de_publicacao = data_de_publicacao.strip()
         
         with open('livros.txt', "a") as arquivo:
             arquivo.write(f"\n{nome} - {autor} - {data_de_publicacao}")
             
-
+    def removerLivros(self):
+        livro_remove = input("Bem vindo a Biblioteca pessoal, digite o nome do livro que você quer remover: ")
+        livro_remove = livro_remove.title().strip()
+        
+        lista_livros = self.pegarLivros()
+        cont = 0
+        for livro in lista_livros:
+            if (livro_remove == livro.nome):
+                lista_livros.pop(cont)
+                self.removeLinha(cont)
+                
+                    
+            cont += 1
+        
+    def removeLinha(self, indice):
+        with open('livros.txt', "r") as arquivo:
+            livroLinhas = arquivo.readlines()
+        
+        livroLinhas.pop(indice)
+        
+        with open("livros.txt", "w") as f:
+            for line in livroLinhas:
+                f.write(line)
+            
+        
 
 livros = Biblioteca()
 
-
 livros.adicionarLivros()
-
-
-   
