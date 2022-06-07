40876ca705975c7ad4cbe02f2ee2f3e62f98cb75
Kawhan <kawhan@DESKTOP-1VQLM4V.localdomain>
2022-06-07 11:46:14 -0300

    commit_files_07/06/2022

--- backend/treinamentoPython/cursoPython/projetoBiblioteca/livrosClass/json-library.py
+++ backend/treinamentoPython/cursoPython/projetoBiblioteca/livrosClass/json-library.py
@@ -15,6 +15,7 @@ class Livros:
         return self._nome == outro_filme_nome
     
     def __ne__(self, outro_filme_nome):
+        
         return self._nome != outro_filme_nome
     
     @property
@@ -26,15 +27,22 @@ class Biblioteca():
     def pegarLivros(self):
         livros = []
                     
-        with open('livros.txt', "r") as arquivo:
-            for linha in arquivo:
-                linha  = linha.strip()
-                livros_montagem = linha.split(' - ')
-                if [0, 1, 2] in livros_montagem:    
-                    book = Livros(livros_montagem[0], livros_montagem[1], livros_montagem[2])
-                    livros.append(book)
-                else:
-                    print("erro")
+        # with open('livros.json', "r") as arquivo:
+        #     for linha in arquivo:
+        #         linha  = linha.strip()
+        #         livros_montagem = linha.split(' - ')
+        #         if [0, 1, 2] in livros_montagem:    
+        #             book = Livros(livros_montagem[0], livros_montagem[1], livros_montagem[2])
+        #             livros.append(book)
+        #         else:
+        #             print("erro")
+        
+        with open("livros.json", "r+", encoding="utf-8") as dados:
+            texto_json = json.load(dados)
+        
+        for data in texto_json:
+            book = Livros(data['Nome'], data['Autor'], data['Data de publicação'])
+            livros.append(book)
         
         return livros
         
@@ -58,23 +66,19 @@ class Biblioteca():
             autor = autor.title().strip()
             data_de_publicacao = data_de_publicacao.strip()
         
+        
         list = []
+        with open("livros.json", "r+", encoding="utf-8") as dados:
+            texto_json = json.load(dados)
         
-         
-        with open('livros.json') as arquivo_json:
-            if arquivo_json:
-                arquivo_json_text = json.load(arquivo_json)
-            
-                
-        
-        for line in arquivo_json_text:
+        for line in texto_json:
             list.append(line)
         
-        with open('livros.json', "w") as arquivo:
+                
+        with open('livros.json', "w", encoding="utf-8") as arquivo:
             livro_dict = {"Nome": nome, "Autor": autor, "Data de publicação": data_de_publicacao}
             list.append(livro_dict)
             text_json = json.dumps(list)
-            print(text_json)
             arquivo.write(text_json)
             
     def removerLivros(self):
@@ -83,26 +87,48 @@ class Biblioteca():
         
         lista_livros = self.pegarLivros()
         cont = 0
+        autorizado = False
         for livro in lista_livros:
             if (livro_remove == livro.nome):
                 lista_livros.pop(cont)
                 self.removeLinha(cont)
+                autorizado = True
                 
-                    
+          
             cont += 1
+
+        if (not autorizado):
+            pergunta = input("Livro não encontrado, deseja fazer a operação de novo? ('SIM') ou ('Não')")
+            pergunta = pergunta.strip().title()
+            if (pergunta != "Não" and pergunta != "Nao"):
+                self.removerLivros()
+
         
     def removeLinha(self, indice):
-        with open('livros.txt', "r") as arquivo:
-            livroLinhas = arquivo.readlines()
+        # with open('livros.txt', "r") as arquivo:
+        #     livroLinhas = arquivo.readlines()
         
-        livroLinhas.pop(indice)
+        # livroLinhas.pop(indice)
         
-        with open("livros.txt", "w") as f:
-            for line in livroLinhas:
-                f.write(line)
+        # with open("livros.txt", "w") as f:
+        #     for line in livroLinhas:
+        #         f.write(line)
+        
+        list = []
+        with open("livros.json", "r+", encoding="utf-8") as dados:
+            texto_json = json.load(dados)
+        
+        for line in texto_json:
+            if line != texto_json[indice]:
+                list.append(line)
+        
+        with open('livros.json', "w", encoding="utf-8") as arquivo:
+            text_json = json.dumps(list)
+            arquivo.write(text_json)
             
         
 
 livros = Biblioteca()
 
-livross = livros.adicionarLivros()
+# livross = livros.listarLivros()
+livros.adicionarLivros()
