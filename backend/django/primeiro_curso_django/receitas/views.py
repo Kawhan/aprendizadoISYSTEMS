79cf244e0b658de27c529667887632d9dad74f7e
kawhan <kawhan.laurindo@i.systems.com.br>
2022-08-18 10:44:39 -0300

    commit_files_18/08/2022

--- backend/django/primeiro_curso_django/receitas/views.py
+++ backend/django/primeiro_curso_django/receitas/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de legumes',
+        3: 'Sorvete',
+        4: 'Bolo de Chocolate'
+    }
+    
+    dados =  {
+        'nome_das_receitas': receitas
+    }
+    
+    return render(request,'index.html', dados)
 
 def receita(request):
     return render(request,'receita.html')
