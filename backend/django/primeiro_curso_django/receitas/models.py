9cd2dadff8e06f7f79b757356fc237470f454b3b
kawhan <kawhan.laurindo@i.systems.com.br>
2022-08-30 16:57:09 -0300

    commit_files_30/08/2022

--- backend/django/primeiro_curso_django/receitas/models.py
+++ backend/django/primeiro_curso_django/receitas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 # Create your models here.
 class Receita(models.Model):
+    pessoa  = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -10,3 +12,4 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    publicada = models.BooleanField(default=False)
--- backend/django/primeiro_curso_django/receitas/views.py
+++ backend/django/primeiro_curso_django/receitas/views.py
@@ -3,7 +3,7 @@ from .models import Receita
 
 # Create your views here.
 def index(request):
-    receitas = Receita.objects.all()
+    receitas = Receita.objects.order_by('-date_receita').filter(publicada=True)
     
     dados =  {
         'receitas': receitas
