730db487acf4f93206591bd256a6b1ba4a36e280
Kawhan Laurindo <kawhan.laurindo@i.systems.com.br>
2022-10-31 16:50:54 -0300

    commit_files_31/10/2022

---
+++ desktop/C++/primeiro_curso_c++/jogo_da_adivinhacao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+    cout << "****************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhação! *" << endl;
+    cout << "****************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "O número secreto é " ;
+    cout << numero_secreto;
+    cout << ". Não conte para ninguém" << endl;
+}
