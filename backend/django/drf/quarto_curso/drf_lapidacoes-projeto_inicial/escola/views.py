516f66e14f562f544593266f76c6c9273de78b4f
Kawhan <kawhanlaurindo@outlook.com>
2023-02-05 03:44:56 -0300

    commit_05/02/2023 Feature: Add reddis cache - commit2

--- backend/django/drf/quarto_curso/drf_lapidacoes-projeto_inicial/escola/views.py
+++ backend/django/drf/quarto_curso/drf_lapidacoes-projeto_inicial/escola/views.py
@@ -1,17 +1,25 @@
-from rest_framework import viewsets, generics
-from rest_framework import status
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from escola.models import Aluno, Curso, Matricula
-from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from escola.serializer import (AlunoSerializer, AlunoSerializerV2,
+                               CursoSerializer,
+                               ListaAlunosMatriculadosSerializer,
+                               ListaMatriculasAlunoSerializer,
+                               MatriculaSerializer)
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
+
 
 class AlunosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os alunos e alunas"""
     queryset = Aluno.objects.all()
+
     def get_serializer_class(self):
         if self.request.version == 'v2':
             return AlunoSerializerV2
         else:
             return AlunoSerializer
+
 
 class CursosViewSet(viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
@@ -22,10 +30,12 @@ class CursosViewSet(viewsets.ModelViewSet):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            response = Response(
+                serializer.data, status=status.HTTP_201_CREATED)
             id = str(serializer.data['id'])
             response['Location'] = request.build_absolute_uri() + id
             return response
+
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     """Listando todas as matrículas"""
@@ -33,15 +43,23 @@ class MatriculaViewSet(viewsets.ModelViewSet):
     serializer_class = MatriculaSerializer
     http_method_names = ['get', 'post', 'put', 'path']
 
+    @method_decorator(cache_page(60))
+    def dispatch(self, *args, **kwargs):
+        return super(MatriculaViewSet, self).dispatch(*args, **kwargs)
+
+
 class ListaMatriculasAluno(generics.ListAPIView):
     """Listando as matrículas de um aluno ou aluna"""
+
     def get_queryset(self):
         queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
         return queryset
     serializer_class = ListaMatriculasAlunoSerializer
 
+
 class ListaAlunosMatriculados(generics.ListAPIView):
     """Listando alunos e alunas matriculados em um curso"""
+
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
         return queryset
--- backend/django/drf/quarto_curso/drf_lapidacoes-projeto_inicial/setup/settings.py
+++ backend/django/drf/quarto_curso/drf_lapidacoes-projeto_inicial/setup/settings.py
@@ -146,3 +146,20 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+REDIS_HOST = os.environ.get('REDIS_HOST', '127.0.0.1')
+REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://{}:{}/1".format(REDIS_HOST, REDIS_PORT),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    # ...
+}
+
+SESSION_ENDING = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
